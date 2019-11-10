565b44323d041db97db2f1a76f8e1ccb2d958885
parenas <parenastrujillo@gmail.com>
2019-11-09 22:36:14 -0600

    primer ejercicio resuelto

--- JS/reto1.js
+++ JS/reto1.js
@@ -1,3 +1,8 @@
 let arr = ['julio', 'pedro', 'vero', 'juan'];
+let newArr = [];
+const items  = arr.map((item)=>{
+    if(item.length>4)
+    newArr.push(item.length);
+});
+console.log(newArr);
 
-console.log(arr);
