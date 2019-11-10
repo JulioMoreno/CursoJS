3d23c904198614e05f89e003a60bedfea1588870
VeronicaOlv <veronica.olvera.cruz@outlook.com>
2019-11-09 22:34:28 -0600

    Ejercicio javascriptMap

--- JS/reto1.js
+++ JS/reto1.js
@@ -1,3 +1,19 @@
-let arr = ['julio', 'pedro', 'vero', 'juan'];
+//let arr = ['julio', 'pedro', 'vero', 'juan'];
+//let res =[];
+//arr.forEach( (item)=>{
+//    res.push(item.length);  
+//});
+//console.log(res);
+//console.log(arr);
 
-console.log(arr);
+
+let arr = ['julio', 'pedro', 'vero', 'juan']; 
+const lengthArr = arr.map(item=>{ 
+    if (item.length>4)
+        return item.length
+
+}).filter(item=>{
+    return item!=undefined;
+});
+
+console.log(lengthArr);
